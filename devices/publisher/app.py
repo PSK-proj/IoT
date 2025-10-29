40e3dfeafe2b0cb510693db7dc10c6916774e9f7
Machoo255 <piotrmiernik255@gmail.com>
2025-10-29 11:54:42 +0100

    Initial commit

---
+++ devices/publisher/app.py
@@ -0,0 +1,21 @@
+import os, time, signal, sys
+
+name = os.environ.get("DEVICE_NAME", "publisher")
+period = float(os.environ.get("HEARTBEAT_SEC", "5"))
+
+running = True
+def stop(sig, frame):
+    global running
+    running = False
+signal.signal(signal.SIGINT, stop)
+signal.signal(signal.SIGTERM, stop)
+
+print(f"[{name}] started (heartbeat every {period}s)", flush=True)
+i = 0
+while running:
+    i += 1
+    print(f"[{name}] alive #{i}", flush=True)
+    time.sleep(period)
+
+print(f"[{name}] stopping", flush=True)
+sys.exit(0)
